9c83885b7e24248234f17c41b1bc2af7db744f37
MAJ-Tech <modiajawo24@gmail.com>
2023-02-17 18:47:42 -0800

    NumbeZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/** main - Entery point*/
+int main(void)
+{
+	int i;
+	/**Beginning of the for loop*/
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
