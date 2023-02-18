294f0ce7c3f3099f28dcccaccc70e738aeeec5b6
MAJ-Tech <modiajawo24@gmail.com>
2023-02-17 18:21:07 -0800

    E an Q are non-alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/** main - Entery point*/
+int main(void)
+{
+	char later;
+
+	for (later = 'a'; later <= 'z'; later++)
+	{
+		if (later == 'e' || later == 'q')
+		{
+			continue;
+		}
+		putchar(later);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c~
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/** main - Entery point*/
+int main(void)
+{
+	char later;
+
+	for (later = 'a'; later <= 'z'; later++)
+		putchar(later);
+	for (later = 'A'; later <= 'Z'; later++)
+		putchar(later);
+	putchar('\n');
+	return (0);
+}
