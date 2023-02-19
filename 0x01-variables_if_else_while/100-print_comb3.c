a01bc0b3fd7cb3a8f44f5f451b6a9e8c686e12af
MAJ-Tech <modiajawo24@gmail.com>
2023-02-19 02:16:22 -0800

    Big Combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *Prints all possible
- *commbinations of two digits
+ * main - Entry point
  *
- *Return: (0)(Success)
+ * Return: Always 0 (Success)
  */
 /**main -Entry point*/
 int main(void)
 {
+	/** @f the fist digit, @s the second digit*/
 	int f, s;
 	/**for loop in a  foor loop*/
 	for (f = '0'; f < '9'; f++)
