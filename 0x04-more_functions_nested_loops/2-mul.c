e504f232998c16aedfe97092c654c9989dec5507
MAJ-Tech <modiajawo24@gmail.com>
2023-02-23 15:16:04 -0800

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - check for digits from 0 to 9
+ *@a: number to be check
+ *@b: second digit
+ *Return: 1 (true) 0 (false)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
