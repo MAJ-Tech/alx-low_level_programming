c4489de216868beb1d2ad375e2fbc2741aa8c444
MAJ-Tech <modiajawo24@gmail.com>
2023-02-27 14:43:17 -0800

    impliment strlen

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-	int a, b;
-	a = 98;
-	b = 42;
-	printf("a=%d b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d b=%d\n", a, b);
+	char *str;
+	int len;
+	str = "my first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,5 @@ int _putchar(char c);
 void reset_to_98(int *n);
 /** Function that switch the value of two integers*/
 void swap_int(int *a, int *b);
+/** Return the length of s string*/
+int _strlen(char *s);
