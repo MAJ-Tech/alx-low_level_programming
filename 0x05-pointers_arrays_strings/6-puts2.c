a755f76c83f8e50bd2838ed9d9ed71cf53112a14
MAJ-Tech <modiajawo24@gmail.com>
2023-02-28 00:29:30 -0800

    Put and jump

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,13 @@ void  puts2(char *str)
 {
 	int i = 0;
 	/** loop through the array to count len*/
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		_putchar(*(str + i));
-		i += 2;
+		if (str[i] != 0)
+		{
+			_putchar(*(str + i));
+			i += 2;
+		}
 	}
 	_putchar('\n');
 }
