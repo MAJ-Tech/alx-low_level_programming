0ccc0919fc60839bc29cb1fb35bb39980e51b06c
MAJ-Tech <modiajawo24@gmail.com>
2023-03-01 09:30:45 -0800

    Implimentation of strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - Concanate two strings.
+ * @dest: destination string.
+ * @src: Source string.
+ * @n: number of bytes to copy from @src.
+ * Return: a pointer to a concanated string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *str = dest, *start = src;
+	int i = 0, j =0;
+
+        while (*src++)
+		i++;
+	while (*dest)
+		dest++;
+	if (n > i)
+		n = i;
+	src = start;
+	for (; j < n; j++)
+		*dest++ = *src++;
+
+        *dest = '\0';
+        return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,5 @@
 int _putchar(char c);
 /** concanates two strings */
 char *_strcat(char *dest, char *src);
+/** iplementation 0f strncat() function */
+char *_strncat(char *dest, char *src, int n);
