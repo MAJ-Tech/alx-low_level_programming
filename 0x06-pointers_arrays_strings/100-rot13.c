2d19a35cdcb8fbc9f22bd7cf15deff8ff15be0d4
MAJ-Tech <modiajawo24@gmail.com>
2023-03-03 18:04:26 -0800

    rod13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * root13 - encrypth a message using .
+ * @s: a string
+ *
+ *Return: a cipher string.
+ */
+char *rot13(char *s)
+{
+	char *str = s;
+	while (*s)
+	{
+		while ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+		{
+			if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
+				*s += 13;
+			else
+				*s -= 13;
+			s++;
+		}
+		s++;
+	}
+	return (str);
+}
