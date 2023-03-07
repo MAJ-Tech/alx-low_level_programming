37d1546a6cbee6d2dd3117d3337a55c36c2c447f
MAJ-Tech <modiajawo24@gmail.com>
2023-03-06 17:09:58 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -9,10 +9,10 @@
 int main(void)
 {
     char *s = "hello, world";
-    char *f = "world";
+    char *f = "orld";
     char *t;
 
-    t = _strpbrk(s, f);
+    t = _strstr(s, f);
     printf("%s\n", t);
     return (0);
 }
