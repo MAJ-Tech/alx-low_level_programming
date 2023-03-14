85cc4fae30b7ea4a1ec9bbf123f45186aba65160
MAJ-Tech <modiajawo24@gmail.com>
2023-03-14 13:17:50 -0700

    good imagination can give you wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - check the lenght of a string.
+ * @s: The string to be check.
+ * Return: int (len of the string.
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+/**
+ * _strdup - dublicate a string @str.
+ * @str: string.
+ * Return: A pointer to a duplcate of @str.
+ */
+char *_strdup(char *str)
+{
+	unsigned int size = _strlen(str), i = 0;
+	char *new_str, *start;
+
+	new_str  = malloc(sizeof(char) * (size + 1));
+	start = new_str;
+
+	while (*str)
+	{
+		new_str[i] = *str;
+		i++, str++;
+	}
+	new_str[i] = '\0';
+
+	return (start);
+}
