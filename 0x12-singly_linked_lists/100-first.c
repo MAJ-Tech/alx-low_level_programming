bb1e578314f8592a90c90fd8081861f96f98e896
MAJ-Tech <modiajawo24@gmail.com>
2023-03-31 17:55:57 -0700

    Constructor Attribute

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * first - Prints a string before main function call.
+ *
+ *Return: void.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
