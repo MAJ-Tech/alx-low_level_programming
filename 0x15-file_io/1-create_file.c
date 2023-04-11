f64bd9fd2da34276c9f2732dbbef233739d51727
MAJ-Tech <modiajawo24@gmail.com>
2023-04-10 18:38:34 -0700

    add a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	file_desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file_desc < 0)
 		return (-1);
-	while (*str++)
+	while (str && *str++)
 		_str_len++;
 
 	num_bytes_write = write(file_desc, text_content, _str_len);
