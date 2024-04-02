f5c692b05f4f4d7191eca8f0a7a8f2aa455eac4b
= <=>
2024-04-02 08:15:38 -0700

    Print a doubly linked list

---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# Doubly linked lists
+## Tasks
+### 0. Print list
+* A function that prints all the elements of a dlistint_t list
+* Prototype: size_t print_dlistint(const dlistint_t *h);
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,21 +2,18 @@
 #define LISTS_H
 
 #include <stdio.h>
-/*
- * struct dlistint_t - A node of a doubly linked list.
- * @n: An int value stored in a node.
- * @next: Pointer to the next node.
- * @prev: Pointer to the previous node.
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Description: You can use paragraphs to describe arguments
- * using this method.
+ * Description: doubly linked list node structure
+ *
  */
 typedef struct dlistint_t
 {
 	int n;
-	/**
-	 * struct dlistint_t - A pointer to the next node.
-	 */
 	struct dlistint_t *next;
 	struct dlistint_t *prev;
 } dlistint_t;
